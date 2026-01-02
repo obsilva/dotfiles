a11122d0af0992bd6e086479051757c20cd2c0db
Otavio Silva <obsilva94@gmail.com>
2026-01-02 18:46:14 -0300

    add lualine

---
+++ nvim/dot-config/nvim/lua/plugins/statusline.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	opts = {
+		-- some options: auto, dracula, material, molokai, moonfly, nightfly, nord, onedark, palenight, etc
+		theme = "auto",
+	},
+}

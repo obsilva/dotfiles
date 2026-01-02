ba489d92c9ef7e16280b96ed48542a096d0ccc90
Otavio Silva <obsilva94@gmail.com>
2026-01-02 17:21:32 -0300

    add more completions and make it better

--- zsh/dot-config/zsh/completions.zsh
+++ zsh/dot-config/zsh/completions.zsh
@@ -1,27 +1,71 @@
-autoload -Uz compinit; compinit
-_comp_options+=(globdots)		# With hidden files
+# Load more completions - https://github.com/zsh-users/zsh-completions
+fpath=(/usr/share/zsh/site-functions $fpath)
 
-# Options
-# setopt GLOB_COMPLETE			# Show autocompletion menu with globs
-setopt MENU_COMPLETE				# Automatically highlight first element of completion menu
-setopt AUTO_LIST					# Automatically list choices on ambiguous completion.
-setopt COMPLETE_IN_WORD			# Complete from both ends of a word.
+# [Shift-Tab] move backwards through the completion menu
+if [[ "${terminfo[kcbt]}" != "" ]]; then
+    bindkey "${terminfo[kcbt]}" reverse-menu-complete
+fi
 
-# Completers
-#zzstyle ':completion:*' completer _complete _extensions _ignored _match _approximate
-zstyle ':completion:*' completer _complete _extensions _approximate
-# Completers configuration...
-# Use cache
+# Based on:
+# https://github.com/Phantas0s/.dotfiles/blob/master/zsh/completion.zsh
+#
+# Check the following guide for additional information:
+# https://thevaluable.dev/zsh-completion-guide-examples/
+
+autoload -U compinit; compinit
+_comp_options+=(globdots) # With hidden files
+
+# setopt GLOB_COMPLETE  # Show autocompletion menu with globs
+setopt MENU_COMPLETE    # Automatically highlight first element of completion menu
+setopt AUTO_LIST        # Automatically list choices on ambiguous completion.
+setopt COMPLETE_IN_WORD # Complete from both ends of a word.
+
+# Ztyle pattern
+# :completion:<function>:<completer>:<command>:<argument>:<tag>
+
+# Define completers
+zstyle ':completion:*' completer _extensions _complete _approximate
+
+# Use cache for commands which use it
 zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$ZDOTDIR/.zcompcache"
-# Allow select in a menu
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+# Complete the alias when _expand_alias is used as a function
+zstyle ':completion:*' complete true
+
+zle -C alias-expension complete-word _generic
+bindkey '^Xa' alias-expension
+zstyle ':completion:alias-expension:*' completer _expand_alias
+
+# Allow you to select in a menu
 zstyle ':completion:*' menu select
+
 # Autocomplete options for cd instead of directory stack
-#zstyle ':completion:*' complete-options true
-# Completion matching
-zstyle ':completion:*' matcher-list '+' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:]}={[:upper:]}'
-#zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' complete-options true
+
+zstyle ':completion:*' file-sort modification
+
+# Colors for the different suggestions
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
+zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
+# Colors for files and directory
+zstyle ':completion:*:*:*:*:default' list-colors "${(s.:.)LS_COLORS}"
+
+# Only display some tags for the command cd
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+# zstyle ':completion:*:complete:git:argument-1:' tag-order !aliases
+
+# Required for completion to be in good groups (named after the tags)
+zstyle ':completion:*' group-name ''
+
+zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+
+# See ZSHCOMPWID "completion matching control"
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 zstyle ':completion:*' keep-prefix true
-zstyle ':completion:*' max-errors 3 numeric
+
 # Use SSH configuration to suggest completions when SSH, SCP, SFTP and others
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'

9009296eaf509a756eaec4df6db68ee1156034d9
Otavio Silva <obsilva94@gmail.com>
2026-01-02 18:08:36 -0300

    additional parsers and event & cmd to lazy load treesitter

--- nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,45 +1,50 @@
 return {
-	-- Parser generator
-	'nvim-treesitter/nvim-treesitter',
-	enabled = true,
-	lazy = false,
-	build = ':TSUpdate',
+	-- Parser generator to boost highlighting
+	"nvim-treesitter/nvim-treesitter",
+	event = "VeryLazy",
+	cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+	build = ":TSUpdate",
 
-	config = function()
-		require('nvim-treesitter').setup {
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				'bash',
-				'dockerfile',
-				'html',
-				'java',
-				'javascript',
-				'ledger',
-				'lua',
-				'markdown', 
-				'sql',
-				'toml',
-				'vim',
-				'vimdoc',
-				'yaml',
-			},
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-				--indent = true,
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-		}
-	end
+	opts = {
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
+		-- Automatically install missing parsers when entering buffer
+		auto_install = true,
+		indent = { enable = true },
+		highlight = { enable = true },
+		folds = { enable = true },
+		-- A list of parser names, or "all"
+		ensure_installed = {
+			"bash",
+			"c,",
+			"c_sharp",
+			"csv",
+			"diff",
+			"dockerfile",
+			"html",
+			"hyprlang",
+			"java",
+			"javadoc",
+			"javascript",
+			"json",
+			"ledger",
+			"lua",
+			"make",
+			"markdown",
+			"markdown_inline",
+			"nginx",
+			"properties",
+			"python",
+			"regex",
+			"sql",
+			"ssh_config",
+			"toml",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"xml",
+			"yaml",
+			"zsh",
+		},
+	},
 }
